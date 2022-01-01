ea17ee5820d88840f8153c23d904fe78b1a000b8
antfromano <antfromano@gmail.com>
2022-01-01 21:30:12 +0000

    added product views and templates

--- works/views.py
+++ works/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Work
 
 # Create your views here.
+
+def all_works(request):
+    """ view to show all works, including sorting and search queries """
+
+    works = Work.objects.all()
+
+    context = {
+        'works': works,
+    }
+
+    return render(request, 'works/works.html', context)
