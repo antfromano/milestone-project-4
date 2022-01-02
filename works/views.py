abb581d112038b91b86bc741e8ec3d61e1e9240c
antfromano <antfromano@gmail.com>
2022-01-02 18:24:56 +0000

    added search functionality for work items

--- works/views.py
+++ works/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Work
 
 # Create your views here.
@@ -7,9 +9,21 @@ def all_works(request):
     """ A view to show all works, including sorting and search queries """
 
     works = Work.objects.all()
+    query = None
+    
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "you didn't enter search criteria")
+                return redirect(reverse('works'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            works = works.filter(queries)
 
     context = {
         'works': works,
+        'search_term': query,
     }
 
     return render(request, 'works/works.html', context)
