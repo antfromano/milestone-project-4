13d5efb7534e34b569f293c8a59314193d35602f
antfromano <antfromano@gmail.com>
2022-01-02 17:02:05 +0000

    added works item functionality

--- works/urls.py
+++ works/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_works, name='works'),
+    path('<work_id>', views.work_item, name='work_item'),
 ]
--- works/views.py
+++ works/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Work
 
 # Create your views here.
 
 def all_works(request):
-    """ view to show all works, including sorting and search queries """
+    """ A view to show all works, including sorting and search queries """
 
     works = Work.objects.all()
 
@@ -13,3 +13,15 @@ def all_works(request):
     }
 
     return render(request, 'works/works.html', context)
+
+
+def work_item(request, work_id):
+    """ A view to show individual work details """
+
+    work = get_object_or_404(Work, pk=work_id)
+
+    context = {
+        'work': work,
+    }
+
+    return render(request, 'works/work_item.html', context)
