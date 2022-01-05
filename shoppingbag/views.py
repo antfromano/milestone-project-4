ef25c1d5f25f4ba1317c9216387e86362c7ac36e
antfromano <antfromano@gmail.com>
2022-01-05 19:21:44 +0000

    updated subtotal functionality to shopping bag

--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
+
 
 def view_shoppingbag(request):
     """ view that renders shoppingbag page """
 
     return render(request, 'shoppingbag/shoppingbag.html')
+
 
 def add_to_shoppingbag(request, work_id):
     """ Add a quantity of the specified product to the shoppingbag """
@@ -20,5 +22,41 @@ def add_to_shoppingbag(request, work_id):
         shoppingbag[work_id] = quantity
 
     request.session['shoppingbag'] = shoppingbag
-    print(request.session['shoppingbag'])
     return redirect(redirect_url)
+
+
+def adjust_shoppingbag(request, work_id):
+
+    quantity = int(request.POST.get('quantity'))
+    shoppingbag = request.session.get('shoppingbag', {})
+
+    if quantity > 0:
+        shoppingbag[work_id] = quantity
+    else:
+        del shoppingbag[work_id]
+        if not shoppingbag[work_id]:
+            shoppingbag.pop(work_id)
+
+        request.session['shoppingbag'] = shoppingbag
+        return redirect(reverse('view_shoppingbag'))
+
+
+def remove_from_shoppingbag(request, work_id):
+    """Remove the item from the shoppingbag"""
+
+    try:
+        quantity = int(request.POST.get('quantity'))
+        shoppingbag = request.session.get('shoppingbag', {})
+
+        if quantity > 0:
+            del shoppingbag[work_id]
+            if not shoppingbag[work_id]:
+                shoppingbag.pop(work_id)
+        else:
+            shoppingbag.pop(work_id)
+
+        request.session['shoppingbag'] = shoppingbag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
