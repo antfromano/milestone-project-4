75d9decc22c681ff8478a812092c63fa8af90da3
antfromano <antfromano@gmail.com>
2022-01-06 16:53:50 +0000

    added notification css, shopping cart preview, and additional messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from works.models import Work
 
 # Create your views here.
 
@@ -12,14 +15,18 @@ def view_cart(request):
 def add_to_cart(request, work_id):
     """ Add a quantity of the specified product to the cart """
 
+    work = get_object_or_404(Work, pk=work_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if work_id in list(cart.keys()):
         cart[work_id] += quantity
+        messages.success(request, f'updated {work.name} quantity to {cart[work_id]}')
+
     else:
         cart[work_id] = quantity
+        messages.success(request, f'added {work.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -27,15 +34,26 @@ def add_to_cart(request, work_id):
 
 def adjust_cart(request, work_id):
 
+    work = get_object_or_404(Work, pk=work_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
-    if quantity > 0:
-        cart[work_id] = quantity
+    if quantity:
+        if quantity > 0:
+            cart[work_id] = quantity
+            messages.success(request, f'updated {work.name} quantity to {cart[work_id]}')
+        else:
+            del cart[work_id]
+            if not cart[work_id]:
+                cart.pop(work_id)
+            messages.success(request, f'removed {work.name} from your cart')
     else:
-        del cart[work_id]
-        if not cart[work_id]:
+        if quantity > 0:
+            cart[work_id] = quantity
+            messages.success(request, f'updated {work.name} quantity to {cart[work_id]}')
+        else: 
             cart.pop(work_id)
+            messages.success(request, f'removed {work.name} from your cart')
 
         request.session['cart'] = cart
         return redirect(reverse('view_cart'))
@@ -45,6 +63,7 @@ def remove_from_cart(request, work_id):
     """Remove the item from the cart"""
 
     try:
+        work = get_object_or_404(Work, pk=work_id)
         quantity = int(request.POST.get('quantity'))
         cart = request.session.get('cart', {})
 
@@ -52,11 +71,14 @@ def remove_from_cart(request, work_id):
             del cart[work_id]
             if not cart[work_id]:
                 cart.pop(work_id)
+            messages.success(request, f'removed {work.name} from your cart')
         else:
             cart.pop(work_id)
+            messages.success(request, f'removed {work.name} from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'error removing work')
         return HttpResponse(status=500)
