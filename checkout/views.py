f8ad56abbff5c8f4e2dc73ed9830b9df09b4b51e
antfromano <antfromano@gmail.com>
2022-01-09 21:41:20 +0000

    added stripe keys to settings.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from cart.contexts import cart_sum 
+
+import stripe
 
 # Create your views here.
 
@@ -10,7 +14,10 @@ def checkout(request):
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment")
         return redirect(reverse('works'))
-
+    
+    current_cart = cart_sum(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
