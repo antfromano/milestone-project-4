bb4bb43b6b71e446f5a8cba417d94e7b4fdabd1a
antfromano <antfromano@gmail.com>
2022-01-12 19:21:05 +0000

    added additional stripe funcitonality to views.py in checkout

--- checkout/views.py
+++ checkout/views.py
@@ -10,6 +10,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment")
@@ -18,12 +21,23 @@ def checkout(request):
     current_cart = cart_sum(request)
     total = current_cart['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+)
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KG3WcCUV5kYkVNmHvUYe4yGuVGphQxL2s1BFAz7hxQyfVsXpJN28L6DCoAADpDiWD4Q2BxTUcAmhYJ31MVQdoX500qY5PRYeU',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
