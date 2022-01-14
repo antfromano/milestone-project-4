258f92b31299299b2e0e38ce1db74f0b6e0193db
antfromano <antfromano@gmail.com>
2022-01-14 20:16:52 +0000

    resolving issue with STRIPE_WH_SECRET in settings.py

--- milestone_project_4/settings.py
+++ milestone_project_4/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
@@ -168,8 +168,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Stripe
-FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
