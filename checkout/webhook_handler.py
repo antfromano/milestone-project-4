eac9561682a1ed7ddb1717356afc8b1b40371372
antfromano <antfromano@gmail.com>
2022-01-15 18:20:28 +0000

    completed webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -46,12 +46,11 @@ class StripeWH_Handler:
                     full_name__iexact=shipping_details.name,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
-                    country__iexact=shipping_details.address.country,
-                    postcode__iexact=shipping_details.address.postal_code,
+                    zipcode__iexact=shipping_details.address.postal_code,
                     town_or_city__iexact=shipping_details.address.city,
                     street_address1__iexact=shipping_details.address.line1,
                     street_address2__iexact=shipping_details.address.line2,
-                    county__iexact=shipping_details.address.state,
+                    state__iexact=shipping_details.address.state,
                     grand_total=grand_total,
                     original_cart=cart,
                     stripe_pid=pid,
@@ -72,17 +71,16 @@ class StripeWH_Handler:
                     full_name=shipping_details.name,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
-                    country=shipping_details.address.country,
-                    postcode=shipping_details.address.postal_code,
+                    zipcode=shipping_details.address.postal_code,
                     town_or_city=shipping_details.address.city,
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
-                    county=shipping_details.address.state,
+                    state=shipping_details.address.state,
                     original_cart=cart,
                     stripe_pid=pid,
                 )
-                for item_id, item_data in json.loads(cart).items():
-                    work = work.objects.get(id=item_id)
+                for work_id, item_data in json.loads(cart).items():
+                    work = Work.objects.get(id=work_id)
                     if isinstance(item_data, int):
                         order_line_item = OrderLineItem(
                             order=order,
