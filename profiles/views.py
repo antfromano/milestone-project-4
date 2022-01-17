1ee23a797350e321c1298a8cdfff66178825b73b
antfromano <antfromano@gmail.com>
2022-01-17 14:17:07 +0000

    added profile form, views and updated template

--- profiles/views.py
+++ profiles/views.py
@@ -10,13 +10,20 @@ def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'profile updated successfully')
+    
     form = UserProfileForm(instance=profile)
-    orders = profile.order.all()
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
