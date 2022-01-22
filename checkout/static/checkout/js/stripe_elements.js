531202c6f09d62da5e769a71ea85b441d84ef353
antfromano <antfromano@gmail.com>
2022-01-22 15:19:42 +0000

    resolved and unified color differences in css and all pages

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,7 +1,6 @@
 /*
-    Core logic/payment flow for this comes from here:
+    core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
-
 */
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
@@ -26,7 +25,7 @@ var style = {
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-// Handle realtime validation errors on the card element
+// handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
@@ -42,7 +41,7 @@ card.addEventListener('change', function (event) {
     }
 });
 
-// Handle form submit
+// handle form submit
 var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
@@ -53,7 +52,7 @@ form.addEventListener('submit', function(ev) {
     $('#loading-overlay').fadeToggle(100);
 
     var saveInfo = Boolean($('#id-save-info').attr('checked'));
-    // From using {% csrf_token %} in the form
+    // from using {% csrf_token %} in the form
     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
     var postData = {
         'csrfmiddlewaretoken': csrfToken,
