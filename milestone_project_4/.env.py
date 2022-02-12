4ac15278a9387aefe8a7c25fb64e60528c90b761
antfromano <antfromano@gmail.com>
2022-02-12 20:20:38 +0000

    updated .env in attempt to resolve issues

--- milestone_project_4/.env.py
+++
@@ -1,6 +0,0 @@
-DEBUG="True"
-SECRET_KEY="q7$$33aq+su=bm967kdc&r8ry6@_v*gxk*vnn))lc@re)1z)w9"
-DATABASE_URL=postgres://rlcazdihmnwdih:f12449e7cb03e5395a85a5197693ccbfd97f757a27704692c1fcd961eba18c41@ec2-34-194-171-47.compute-1.amazonaws.com:5432/dfqekamunu242m
-SQLITE_URL=sqlite:///my-local-sqlite.db
-CACHE_URL=memcache://127.0.0.1:11211,127.0.0.1:11212,127.0.0.1:11213
-REDIS_URL=rediscache://127.0.0.1:6379/1?client_class=django_redis.client.DefaultClient&password=ungithubbed-secret
--- milestone_project_4/settings.py
+++ milestone_project_4/settings.py
@@ -18,6 +18,9 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
+# reading .env file
+environ.Env.read_env()
+
 # Set the project base directory
 # build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
