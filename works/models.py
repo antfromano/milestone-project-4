0479c12fb9c5061f12bf37ab669275ed4b9adb12
antfromano <antfromano@gmail.com>
2022-02-20 16:06:10 +0000

    updated models.py in works

--- works/models.py
+++ works/models.py
@@ -39,7 +39,6 @@ class Review(models.Model):
     user_rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True, default=1, validators=[
                                  MaxValueValidator(5), MinValueValidator(1)])
     comment = models.CharField(max_length=200, default='')
-    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.work
