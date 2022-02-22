2674b75ccaccadac630ef18d2195a7988ed90011
antfromano <antfromano@gmail.com>
2022-02-22 13:37:38 +0000

    added class ReviewForm to forms.py in works

--- works/forms.py
+++ works/forms.py
@@ -17,3 +17,9 @@ class WorkForm(forms.ModelForm):
 
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = '__all__'
